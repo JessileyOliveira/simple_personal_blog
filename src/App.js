a67ef8cda58e4783823e539719b67e3ce80c70c8
Jessiley <xjaco222x@hotmail.com>
2020-05-13 14:20:19 -0300

    Feat: Added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Routes from './routes';
 import './App.css';
-
-import person from './assets/images/person.jpeg';
 
 function App() {
   return (
     <>
-      <h1>Hello Axur</h1> <img src={person} alt="person" />
+      <Routes />
     </>
   );
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+  return <h1>Hello Axur</h1>;
+}
+
+export default Home;
