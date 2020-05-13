3852c74f22ef68d481131873cb2b0c2184df7429
Jessiley <xjaco222x@hotmail.com>
2020-05-13 13:40:23 -0300

    Feat: basic configuration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import person from './assets/images/person.jpeg';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Axur</h1> <img src={person} />
+    </>
+  );
+}
+
+export default App;
